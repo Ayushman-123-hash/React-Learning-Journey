45e2b538124fc803c2e9694d46c2251a19e2ee82
Ayushman-123-hash <ayushman4544@gmail.com>
2026-06-14 13:07:24 +0530

    Currency Converter

---
+++ 06_Currency_Converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+
+        fetch(`https://v6.exchangerate-api.com/v6/ab44fc165840760fe149b5b2/latest/${currency}`)
+            .then((res) => res.json())
+            .then((res) => setData(res.conversion_rates))
+
+    }, [currency])
+
+    return data
+}
+
+export default useCurrencyInfo
+
